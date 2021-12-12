41d1fb994c424c8fcb395c2d4624e6e69202c188
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-12-12 22:27:23 +0600

    fixing issue with node modules

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
 
+
+// Show modal, Focus on Input
+const showModal = () => { 
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal')); 
