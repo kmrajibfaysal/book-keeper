abca29a532603aa8631113f076024ecfcc3f57ff
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-12-13 11:15:41 +0600

    adding javascript validation and other stuff for form validation

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
+/* eslint-disable no-unused-expressions */
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
-var bookmarksContainer = document.getElementById('bookmarks-container');
-
+const bookmarksContainer = document.getElementById('bookmarks-container');
 
 // Show modal, Focus on Input
-const showModal = () => { 
+const showModal = () => {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
-}
+};
 
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
-modalClose.addEventListener('click', () => modal.classList.remove('show-modal')); 
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => {
+    e.target === modal ? modal.classList.remove('show-modal') : false;
+});
+
+// validate form
+function validate(nameValue, urlValue) {
+    const expression =
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit value for both fields.');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Handle Data from form
+const storeBookmark = (e) => {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+};
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
